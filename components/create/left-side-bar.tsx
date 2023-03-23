237d7b035916814cf13de455533aa411c06f8259
Mark <markrachapoom@gmail.com>
2023-03-23 23:04:26 +0700

    Refined left side bar

--- components/create/left-side-bar.tsx
+++ components/create/left-side-bar.tsx
@@ -1,52 +1,87 @@
+'use client';
+
+import React, { useEffect } from 'react'
+
+// next
 import Link from 'next/link';
-import React from 'react'
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+
+import { FiMoreHorizontal } from "react-icons/fi";
 
 type Props = {}
 
 const createRoutes = [
   // character, storyboard, cover, styles, review.
   {
-    path: '/create/characters',
-    pathname: 'Characters',
+    pathname: '/create/characters',
+    path: 'Characters',
   },
   {
-    path: '/create/storyboard',
-    pathname: 'Storyboard',
+    pathname: '/create/storyboard',
+    path: 'Storyboard',
   },
   {
-    path: '/create/cover',
-    pathname: 'Cover',
+    pathname: '/create/cover',
+    path: 'Cover',
   },
   {
-    path: '/create/styles',
-    pathname: 'Styles',
+    pathname: '/create/styles',
+    path: 'Styles',
   },
   {
-    path: '/create/review',
-    pathname: 'Review',
+    pathname: '/create/review',
+    path: 'Review',
   },
 ];
 
-const LeftSideBar = (props: Props) => {
+const LeftSideBar: React.FC<{}> = ({}) => {
+
+  const pathName = usePathname();
+
   return (
     <div className='flex flex-col justify-between w-full h-full border-r border-r-light-divider dark:border-r-dark-divider'>
 
       <div>
         { createRoutes.map((route, index) => {
           return (
-            <Link key={index} href={route?.path}>
+            <Link key={index} href={route?.pathname}>
               <button 
-                className='flex items-center justify-center w-full h-16 border-b border-b-light-divider dark:border-b-dark-divider hover:dark:bg-dark-background-secondary hover:bg-light-background-secondary transition-all active:opacity-50'
+                className={
+                  clsx('flex items-center justify-center w-full h-16 border-b border-b-light-divider dark:border-b-dark-divider transition-all active:opacity-50',
+                    // { 'hover:dark:bg-dark-background-secondary hover:bg-light-background-secondary': !isActive },
+                    { 'dark:bg-dark-background-secondary bg-light-background-secondary text-light-text-primary dark:text-dark-text-primary': pathName === route.pathname },
+                    { 'text-light-text-tertiary dark:text-dark-text-tertiary': pathName !== route.pathname}
+                  )
+                }
               >
-                <span>{route.pathname}</span>
+                <span className='font-semibold'>{route.path}</span>
               </button>
             </Link>
           )
         })}
       </div>
 
-      <div>
-        <span>Account</span>
+      <div className='flex flex-row justify-between p-3 items-center border-t border-t-light-divider dark:border-t-dark-divider'>
+
+        <div className='flex flex-row gap-3 items-center'>
+
+          {/* @ts-ignore */}
+          <img
+            src='https://pbs.twimg.com/profile_images/1631949874001498113/At1b9Wrr_400x400.jpg'
+            className='rounded-full w-9 h-9'
+            alt='profile image'
+          />
+
+          <div className=' flex flex-col gap-0'>
+            <span className='text-sm'>Mark</span>
+            <span className='text-light-text-secondary dark:text-dark-text-secondary text-sm'>Edit Profile</span>
+          </div>
+        </div>
+
+        <FiMoreHorizontal className='text-light-text-secondary dark:text-dark-text-secondary'/>
+
       </div>
     </div>
   )
