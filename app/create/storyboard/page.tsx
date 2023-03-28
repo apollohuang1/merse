d46aecfb3dc9b45a0041787368376d7f2be1d907
Mark <markrachapoom@gmail.com>
2023-03-28 20:35:25 +0700

    Fix /storyboard layout and overflow behavior

--- app/create/storyboard/page.tsx
+++ app/create/storyboard/page.tsx
@@ -1,27 +1,33 @@
+'use client'
+
 import CreateHeader from "@/components/create/create-header";
 import MaxWidthContainer from "@/components/create/max-width-container";
 import { styleSamples } from "@/util/characters";
 import React from "react";
 import { FiArrowRight } from "react-icons/fi";
+import { createRoutes } from "../layout";
 
 type Props = {};
 
 const Storyboard = (props: Props) => {
   return (
-    <div className="flex flex-col w-full justify-between">
+
+    <div className="grid grid-rows-[100px_auto] overflow-auto">
+      {/* top of grid */}
+      <CreateHeader currentRoute={createRoutes[2]}/>
 
       {/* main content */}
-      <div className="grid grid-cols-2 w-full h-full">
+      <div className="grid grid-cols-2 w-full h-[calc(100vh-100px)]">
         
         {/* prompt panel */}
-        <div className="w-full h-full">
+        <div className="w-full h-full overflow-hidden">
           <textarea
-            placeholder="Type a prompt..."
-            className="w-full h-full bg-transparent p-7 placeholder:text-light-text-tertiary dark:placeholder:text-dark-text-tertiary"
+            placeholder="Type a full journaling prompt..."
+            className="w-full h-full bg-transparent p-7 placeholder:text-light-text-tertiary dark:placeholder:text-dark-text-tertiary focus:outline-none"
           />
         </div>
 
-        <div className="flex flex-col w-full h-full">
+        <div className="flex flex-col w-full h-full overflow-scroll">
           <div className="flex flex-col max-md:flex max-md:flex-col gap-3 w-full h-full">
             {/* story card */}
             {styleSamples.map((style, index) => (
@@ -51,6 +57,7 @@ const Storyboard = (props: Props) => {
           </div>
         </div>
       </div>
+
     </div>
   );
 };
