e20aba56efa2ac81b23b56d3cee354b3aba4ef02
Mark <markrachapoom@gmail.com>
2023-03-31 22:56:35 +0700

    Revert "Revert "Test new route from Next/Web API approach""
    This reverts commit e7ba19b1bc22f18bb732bfa74d343884fee25e3d.

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,8 @@
+
 export async function GET(request: Request) {
+  return new Response('Hello, Next.js!')
+}
+
+export async function POST(request: Request) {
   return new Response('Hello, Next.js!')
 }
