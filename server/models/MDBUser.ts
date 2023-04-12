eae8134d34e28360f25d0199989a1a11e5ea162b
Mark <markrachapoom@gmail.com>
2023-04-12 22:29:10 +0700

    Change user schema

--- server/models/MDBUser.ts
+++ server/models/MDBUser.ts
@@ -1,10 +1,17 @@
 import mongoose, { Schema, SchemaType } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  profile_image_url: string;
+}
+
+const UserSchema = new mongoose.Schema<IUser>({
   _id: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true },
   profile_image_url: { type: String, required: false },
 });
 
-export default mongoose.model('User', UserSchema);
+export default mongoose.model<IUser>('User', UserSchema);
