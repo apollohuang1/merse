2af59054e3160e9fe964a4eba584f016b0c9fa9a
Mark <markrachapoom@gmail.com>
2023-04-17 09:29:55 +0700

    Create layout page for emily

---
+++ app/create/layout/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const LayoutPage = (props: Props) => {
+  return (
+    <div>Hello emily ❤️, this page for you!</div>
+  )
+}
+
+export default LayoutPage;
