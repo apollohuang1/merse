a40279da142c181bcdc601560a66724dae9e8c42
mlapark <emily.park@berkeley.edu>
2023-04-17 17:40:47 -0700

    Update page.tsx

--- app/create/layout/page.tsx
+++ app/create/layout/page.tsx
@@ -1,117 +1,213 @@
 "use client";
 
-import React, { useState } from "react";
-import createImageFromText from "@/hooks/useCreateEntry";
-import { FiArrowRight } from "react-icons/fi";
 import CreateHeader from "@/components/create/create-header";
-import MaxWidthContainer from "@/components/create/max-width-container";
-import { createRoutes } from "@/util/create-constants";
+import React, { FormEventHandler, useState } from "react";
+import {
+  FiBold,
+  FiCode,
+  FiEdit2,
+  FiImage,
+  FiItalic,
+  FiList,
+} from "react-icons/fi";
+import { BsQuote } from "react-icons/bs";
+import { IoText } from "react-icons/io5";
+
+import {
+  useEditor,
+  EditorContent,
+  FloatingMenu,
+  JSONContent,
+  BubbleMenu,
+  Editor,
+} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Image from "@tiptap/extension-image";
+
+// ChakraUI
+import { Spinner } from "@chakra-ui/react";
+
+// import editorStyles from "../../../styles/editor.module.css";
+import clsx from "clsx";
+import Modal from "@/components/modal";
+import Placeholder from "@tiptap/extension-placeholder";
+import { createRoutes, storyboardSamples } from "@/util/create-constants";
+
+import { useAppDispatch, useAppSelector } from "@/redux-store/hooks";
+import {
+  setContent,
+  setShowGeneratedStoryboard,
+  setTitle,
+} from "@/redux-store/store";
+import useCreateEntry from "@/hooks/useCreateEntry";
+import Blockquote from "@tiptap/extension-blockquote";
+import { Scene } from "@/models/entry";
 
 type Props = {};
 
 const LayoutPage = (props: Props) => {
-    // State variable to store the image URL
-    const [imageURL, setImageURL] = useState("");
+  // redux states
+  const entry = useAppSelector((state) => state.entry);
+  const entryHelper = useAppSelector((state) => state.entryHelper);
+  const dispatch = useAppDispatch();
+
+  const [showAddingImageModal, setShowAddingImageModal] = React.useState<boolean>(false);
+  const [addingImageURL, setAddingImageURL] = React.useState<string>("");
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        heading: {
+          HTMLAttributes: {
+            class: "text-light-text-primary dark:text-dark-text-primary",
+          },
+          levels: [1, 2, 3],
+        },
+      }),
+      Placeholder.configure({
+        placeholder: "Press tab or click to select a menu item...",
+      }),
+      Image.configure({
+        inline: true,
+        allowBase64: true,
+        HTMLAttributes: {},
+      }),
+      Blockquote.configure({
+        HTMLAttributes: {
+          class:
+            "bg-light-background-secondary dark:bg-dark-background-secondary rounded-none p-4 border-l-2 border-emerald-500",
+        },
+      }),
+    ],
+    editorProps: {
+      attributes: {
+        class:
+          "outline-none w-full h-full bg-transparent min-h-[calc(100vh-300px)] highlight selection:bg-[#3cc9a3] selection:bg-opacity-25",
+      },
+    },
+  });
+
+  editor?.on("update", (updatedEditor: any) => {
+    const updatedContent = updatedEditor?.editor?.getJSON();
+    dispatch(setContent(updatedContent));
+  });
+
+  editor?.on("create", (createdEditor: any) => {
+    createdEditor?.editor.commands.setContent(entry?.content);
+  });
 
   return (
-    <div className="grid grid-rows-[100px_auto] overflow-auto">
-      {/* top of grid */}
-      <CreateHeader currentRoute={createRoutes[3]} />
+    <>
+      <div className="grid grid-rows-[100px_auto] overflow-auto">
+        {/* navigation header */}
+        <CreateHeader currentRoute={createRoutes[2]} />
 
-      <div className="flex flex-col w-full h-full justify-center items-center">
-        <div className="flex flex-col w-full h-full justify-between items-center max-w-3xl py-6">
-          {/* book cover */}
-          <img
-            src={imageURL} // Use the imageURL in the src attribute
-            className="object-cover rounded-md aspect-[2/3] w-[400px]"
-            alt="comic book cover"
-          />
+        {/* main content (left and right panels columns) */}
+        <div
+          className={clsx(
+            "flex flex-row w-full h-full overflow-hidden px-7",
+            { "gap-0": !entryHelper.showGeneratedStoryboard },
+            { "gap-7": entryHelper.showGeneratedStoryboard }
+          )}
+        >
+          {/* right panel */}
+          <div
+            className={clsx(
+              "flex flex-col h-full items-center duration-300 overflow-auto gap-4 flex-shrink-0",
+              { "w-0 opacity-0": !entryHelper.showGeneratedStoryboard },
+              { "w-[400px]": entryHelper.showGeneratedStoryboard }
+            )}
+          >
+            <div className="flex flex-col w-full gap-4 max-xl:flex max-xl:flex-col">
+              {
+                entry?.scenes.map((scene: Scene, index: number) => (
+                  <div
+                  key={index}
+                  className="group relative flex flex-col w-full bg-light-background-secondary dark:bg-dark-background-secondary border border-light-divider dark:border-dark-divider aspect-auto min-w-[400px]"
+                >
+                  {/* overlay  */}
+                  <div className="flex absolute w-full h-full items-center justify-center aspect-squar bg-black bg-opacity-30 dark:bg-opacity-30 opacity-0 group-hover:opacity-100 group-active:opacity-50 transition-all rounded-lg cursor-pointer">
+                    <FiEdit2 className="w-9 h-9 text-white" />
+                  </div>
+
+                  <img
+                    src={"data:image/png;base64," + scene.image_base64 }
+                    alt="comic book cover"
+                    className="object-cover aspect-[4/3]"
+                  />
+
+                  {/* story line in storyboard */}
+                  <div className="flex p-4">
+                    <p className="text-light-text-primary dark:text-dark-text-primary line-clamp-[8]">
+                      {scene.text}
+                    </p>
+                  </div>
+                </div>
+                ))
+              }
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+
+      <Modal
+        isOpen={showAddingImageModal}
+        onClose={() => setShowAddingImageModal(false)}
+        withCloseButton={false}
+        title="Embed Image"
+      >
+        <div className="flex flex-col w-full h-full px-3 pb-3">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (editor) {
+                editor.chain().focus().setImage({ src: addingImageURL }).run();
+              }
+              setShowAddingImageModal(false);
+              setAddingImageURL("");
+            }}
+          >
+            <input
+              type="text"
+              value={addingImageURL}
+              placeholder="Paste the image link..."
+              onChange={(e) => setAddingImageURL(e.target.value)}
+              className="flex flex-row w-full h-11 px-4 rounded-lg bg-transparent border border-light-divider dark:border-dark-divider focus:outline-accent focus:outline-1 outline-none transition-all text-light-text-primary dark:text-dark-text-primary placeholder:text-light-text-tertiary dark:placeholder:text-dark-text-tertiary"
+            />
+          </form>
+
+          {/* cancel and add buttons on the right hand side */}
+          <div className="flex flex-row w-full h-12 gap-2 mt-4 justify-end">
+            <button
+              onClick={() => {
+                setShowAddingImageModal(false);
+              }}
+              className="flex flex-row h-10 w-20 rounded-full outline-none transition-all text-light-text-primary dark:text-dark-text-primary items-center justify-center"
+            >
+              Cancel
+            </button>
+
+            <button
+              onClick={() => {
+                if (editor) {
+                  // editor.chain().focus().setImage({ src: "https://i.ytimg.com/vi/U1VcEgS0XkQ/maxresdefault.jpg" }).run()
+                  editor
+                    .chain()
+                    .focus()
+                    .setImage({ src: addingImageURL })
+                    .run();
+                }
+                setShowAddingImageModal(false);
+              }}
+              className="flex flex-row h-10 w-20 rounded-full bg-accent border border-accent focus:outline-accent outline-none transition-all text-light-text-primary dark:text-dark-text-primary items-center justify-center"
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
 export default LayoutPage;
-
-// "use client";
-
-// import React, { useState, useEffect } from "react";
-// import { FiEdit2, FiArrowRight } from "react-icons/fi";
-// import CreateHeader from "@/components/create/create-header";
-// import MaxWidthContainer from "@/components/create/max-width-container";
-// import { createRoutes } from "@/util/create-constants";
-// import { useDispatch } from 'react-redux';
-// import { addScene, setIsGeneratingStoryboard, setShowGeneratedStoryboard } from "@/redux-store/store";
-
-// type Props = {
-//   entry: {
-//     scenes?: Scene[];
-//   };
-// };
-
-// type Scene = {
-//   _id?: string; // Make _id property optional
-//   image_base64: string;
-//   text: string;
-// };
-
-// const newScene = {
-//   _id: "", // Add default value for _id property
-//   image_base64: "hello1",
-//   text: ""
-// };
-
-// const LayoutPage = ({ entry }: Props) => {
-//   const dispatch = useDispatch();
-//   const [imageURLs, setImageURL] = useState(
-//     entry?.scenes?.map(scene => scene.image_base64) ?? []
-//   );
-
-//   useEffect(() => {
-//     const sceneToAdd = { ...newScene };
-//     dispatch(addScene(sceneToAdd));
-//     setGeneratedScene(sceneToAdd); // Update directly with the newScene object
-//   }, []);
-
-//   console.log(imageURLs);
-
-//   return (
-//     <div className="grid grid-rows-[100px_auto] overflow-auto">
-//       {/* top of grid */}
-//       <CreateHeader currentRoute={createRoutes[3]} />
-
-//       <div className="flex flex-col w-full h-full justify-center items-center">
-//         <div className="flex flex-col w-full h-full justify-between items-center max-w-3xl py-6">
-//           {/* book covers */}
-//           {imageURLs.map((imageURL, index) => (
-//             <div
-//               key={index}
-//               className="relative flex flex-col w-full bg-light-background-secondary dark:bg-dark-background-secondary border border-light-divider dark:border-dark-divider aspect-auto min-w-[400px] mb-4"
-//             >
-//               {/* overlay */}
-//               <div className="absolute w-full h-full items-center justify-center aspect-squar bg-black bg-opacity-30 dark:bg-opacity-30 opacity-0 cursor-pointer transition-all rounded-lg">
-//                 <FiEdit2 className="w-9 h-9 text-white" />
-//               </div>
-
-//               <img
-//                 src={"data:image/png;base64," + imageURL}
-//                 alt="comic book cover"
-//                 className="object-cover aspect-[4/3]"
-//               />
-
-//               {/* story line in storyboard */}
-//               <div className="flex p-4">
-//                 <p className="text-light-text-primary dark:text-dark-text-primary line-clamp-[8]">
-//                   {entry.scenes?.[index].text}
-//                 </p>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LayoutPage;
