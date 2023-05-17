b43601d2577e00105e71259f12de35bbc132e96c
Emily Park <emily.park@berkeley.edu>
2023-05-16 21:37:49 -0700

    Create types.ts

---
+++ models/types.ts
@@ -0,0 +1,15 @@
+export type StyleReference = {
+    artist: string;
+    artwork: {
+      url: string;
+    };
+    id?: string;
+  };
+
+export type ComicStyle = {
+    artist: string;
+    artwork: {
+      url: string;
+    };
+    id?: string;
+  };
