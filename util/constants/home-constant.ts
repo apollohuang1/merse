e6210485b37c6f3845a5cf6eeea36bf7f5e3ce7f
Mark <markrachapoom@gmail.com>
2023-05-30 19:51:05 +0700

    Update only profile url

--- util/constants/home-constant.ts
+++ util/constants/home-constant.ts
@@ -45,8 +45,7 @@ export const sampleArtists: SampleArtist[] = [
     name: "Mark",
     username: "mark",
     _id: "6436f3032b67ae01b9c884bb",
-    profile_image_url:
-      "https://pbs.twimg.com/profile_images/1631949874001498113/At1b9Wrr_400x400.jpg",
+    profile_image_url: "https://pbs.twimg.com/profile_images/1660337859507920896/RHcrAUsz_400x400.jpg",
   },
   {
     name: "Emily",
