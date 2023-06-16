fb4a59ac8f8921bf1c30772abeea7b8b14dcb380
Mark <markrachapoom@gmail.com>
2023-06-16 11:54:03 +0700

    1 liner loop

--- hooks/useCreateEntry.ts
+++ hooks/useCreateEntry.ts
@@ -202,7 +202,8 @@ const useCreateEntry = () => {
 
       // 🚨 Comment this out to generate the entire storyboard. This will burn a lot of the API quota.
       // iterate through splitedSceneText array
-      for (let i = 0; i < sceneTextsArray.length; i++) {
+      // for (let i = 0; i < sceneTextsArray.length; i++) {
+      for (let i = 0; i < 2; i++) {
         const base64String = await createImageFromText(sceneTextsArray[i]);
         const newScene: Scene = {
           _id: new mongoose.Types.ObjectId().toString(),
